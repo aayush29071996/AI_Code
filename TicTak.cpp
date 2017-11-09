641ec2c75d8f21341faa6783d25ba57413058b69
Aayush Patel <aayush29071996@gmail.com>
2017-11-09 21:01:15 +0530

    Update TicTak.cpp

--- TicTak.cpp
+++ TicTak.cpp
@@ -69,7 +69,7 @@ int main() {
 
         // Checking for  row win
         for(int i = 0; i< 3; i++) {
-            if(board[i][0] == board[i][1] && board[i][2] == board[i][1] && board[i][0] !=' '){
+            if(board[i][0] == board[i][1] && board[i][2] == board[i][1] && board[i][1] !=' '){
                 cout<<board[i][0]<<" won!";
                 gameOver = true;
                 break;
@@ -77,14 +77,14 @@ int main() {
         }
         // Checking for  col win
         for(int i = 0; i< 3; i++) {
-            if(board[0][i] == board[1][i] && board[2][i] == board[1][i] && board[0][i] !=' '){
+            if(board[0][i] == board[1][i] && board[2][i] == board[1][i] && board[1][i] !=' '){
                 cout<<board[0][i]<<" won!";
                 gameOver = true;
                 break;
             }
         }
         // Checking for  diagonal win
-        if(board[0][0] == board[1][1] && board[2][2] == board[1][1] && board[0][0] !=' '){
+        if(board[0][0] == board[1][1] && board[2][2] == board[1][1] && board[1][1] !=' '){
             cout<<board[1][1]<<" won!";
             gameOver = true;
             break;
